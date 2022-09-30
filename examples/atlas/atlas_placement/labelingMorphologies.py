21a72dcd21456f7a3646acb354df4a4dd49416b5
claudia casellato <claudia.casellato@unipv.it>
2022-09-30 16:22:47 +0200

    Selection of changes without Binary Large Objects or verbose logs in out

---
+++ examples/atlas/atlas_placement/labelingMorphologies.py
@@ -0,0 +1,62 @@
+import numpy as np
+from bsb.core import from_hdf5
+from bsb.storage import Storage
+from bsb.config import from_json
+from bsb.core import Scaffold
+from bsb.morphologies import Morphology
+import h5py
+from bsb.plotting import plot_morphology
+from bsb.plotting import get_branch_trace
+import plotly.graph_objects as go
+#cell = Morphology.from_swc("morphologies/PyramidalCell.swc")
+
+####or
+net=from_hdf5("VISp.hdf5")
+mr = net.morphologies
+cell = mr.load("test1")
+# print(np.sum(cell.tags == 1))
+# print(np.sum(cell.tags == 2))
+# print(np.sum(cell.tags == 3))
+# print(np.sum(cell.tags == 4))
+
+print("numb of branches on the morphology: ", len(cell.branches))
+print("numb of points on the morphology: ", len(cell.tags))
+print(cell.labelsets)
+#plot_morphology(cell)
+# print(cell.tags)
+#
+#
+# branches = [b for b in cell.branches if np.all(b.tags == 3)]
+# for b in branches:
+#     b.label(["basal_dendrites"])
+# branches = [b for b in cell.branches if np.all(b.tags == 4)]
+# for b in branches:
+#     b.label(["apical_dendrites"])
+#### to add specific labels on specific points
+#cell.label(["my_label"], [15, 30, 45])
+
+# dendrites = cell.labels.get_mask(["dendrites"])
+# dendrites
+# apical_dendrites = cell.get_branches(labels=["apical_dendrites"])
+# # # np.all(dendrites == (pc.tags == 3))
+# for b in apical_dendrites:
+#     b.label(["dendrites"])
+# #cell.labels.label(["dendrites", "labels"], apical_dendrites)
+#
+# mr.save("test2", cell)
+
+
+####### PLOTTING
+plot_morphology(cell, color={"soma": "black", "axon": "red", "dendrites": "blue"})
+
+
+# fig = go.Figure()
+# color="black"
+# width=1.0
+# traces = []
+# for branch in cell.branches:
+#     print(branch.label)
+#     traces.append(get_branch_trace(branch, offset=None, color=color, width=width))
+# for trace in traces:
+#     fig.add_trace(trace)
+# fig.show()
